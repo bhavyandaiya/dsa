d678db76f240b2968b4a2efeeaa66789c5024675
Bhavyan Daiya <daiyabhavyan@gmail.com>
2026-03-18 11:23:40 -0600

    selection

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,6 +1,7 @@
 nums = [5, 7, 8, 8, 4, 1, 6, 9, 2]
 
-def selection_sort(nums):
+# ASCENDING:
+def selection_sort_asc(nums):
     n = len(nums)
     for i in range(0, n):
         mini_ind = i
@@ -9,5 +10,21 @@ def selection_sort(nums):
                 mini_ind = j
         nums[i], nums[mini_ind] = nums[mini_ind], nums[i]
     return nums
+print(selection_sort_asc(nums))
 
-print(selection_sort(nums))
+
+# DESCENDING
+
+# nums = [5, 7, 8, 8, 4, 1, 6, 9, 2]
+def selection_sort_des(nums):
+    n = len(nums)
+    for i in range(0, n):
+        max_ind = i
+        for j in range(i + 1, n):
+            if nums[j] > nums[max_ind]:
+                max_ind = j
+        nums[i], nums[max_ind] = nums[max_ind], nums[i]
+    return nums
+
+
+print(selection_sort_des(nums))
